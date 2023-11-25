f34c702ed9d6a0342f8f2970b5227b1d025921b6
Sakebul-islam <mdsakebul66@gmail.com>
2023-11-25 21:07:33 +0600

    jwt tocan creation api create successfully

--- index.js
+++ index.js
@@ -44,6 +44,22 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // auth related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      console.log('I need a new jwt', user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '15d',
+      });
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true });
+    });
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
     console.log(
